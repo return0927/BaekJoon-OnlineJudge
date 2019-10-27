d23398c1f0a6385fc0b6be8c4dd695089c41e720
EunhakLee <admin@return0927.xyz>
2019-10-27 16:31:31 +0900

    Add BOJ2581 (Wrong)

---
+++ BOJ/2581/2581.py
@@ -0,0 +1,19 @@
+def check_prime(n: int) -> bool:
+    if n == 1:
+        return False
+    for i in range(2, int(n**.5 + .5)):
+        if n % i == 0:
+            return False
+    return True
+
+
+# 1 <= under, over <= 10000
+under, over = int(input()), int(input())
+
+primes = [*filter(lambda x: check_prime(x), [*range(under, over + 1)])]
+
+if primes:
+    print(sum(primes))
+    print(primes[0])
+else:
+    print(-1)
