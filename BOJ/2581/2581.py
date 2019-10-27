f0c9f8b26f8ddb0a04581ba6bd1d5352eaee0a62
EunhakLee <admin@return0927.xyz>
2019-10-27 16:43:18 +0900

    Solve BOJ2581
    In checking the number `n` is prime, it could be determined with the
    remains with dividing i, ranging from 2 to sqrt(n). Prior solution
    considered in range(2, int(sqrt of n + .5)), so can be round up, but
    python's ranging end is '<' not '<='.

--- BOJ/2581/2581.py
+++ BOJ/2581/2581.py
@@ -1,7 +1,7 @@
 def check_prime(n: int) -> bool:
     if n == 1:
         return False
-    for i in range(2, int(n**.5 + .5)):
+    for i in range(2, int(n**.5 + .5) + 1):
         if n % i == 0:
             return False
     return True
