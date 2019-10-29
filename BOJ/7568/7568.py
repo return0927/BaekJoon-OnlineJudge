ce21082a6b537f28406f133864bfab30677b9bd5
EunhakLee <admin@return0927.xyz>
2019-10-29 15:25:58 +0900

    Solve BOJ7568
    In addition, the function `rank` is not used, but it could be used upcoming problems.
    So I wouldn't remove it

---
+++ BOJ/7568/7568.py
@@ -0,0 +1,35 @@
+def rank(entire: list):
+    """동일한 값을 높은 등수로, 동알힌 값의 개수 반영 랭킹구하기"""
+    ranks = [0, ] * len(entire)
+    upper = sorted(entire)
+
+    for i in range(len(entire)):
+        ranks[i] = upper.index(entire[i]) + 1
+
+    return ranks
+
+
+N = int(input())
+
+humans = []
+for n in range(N):
+    weight, height = map(int, input().split(' '))
+    humans.append((weight, height))
+
+# 전수조사 비교를 위한 2차원 배열 생성
+matrix = [[False for x in range(N)] for y in range(N)]
+
+# print(matrix)
+# 전수조사 비교 시행
+for i in range(N):
+    mw, mh = humans[i]
+    for j in range(N):
+        w, h = humans[j]
+
+        if i == j:
+            matrix[j][i] = None
+        elif mw > w and mh > h:
+            matrix[j][i] = True
+
+ranks = [x.count(True) + 1 for x in matrix]
+print(" ".join(map(str, ranks)))
---
+++ BOJ/7568/README.md
@@ -0,0 +1,9 @@
+# BOJ7568 - None
+
+> [View in BOJ](https://www.acmicpc.net/problem/7568)
+
+## Solutions
+- [Solution on Python](7568.py)
+
+
+_Page built: Oct 29 2019 06:26:03 (W+43, UTC)_
