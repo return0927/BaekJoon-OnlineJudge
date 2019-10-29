2a015346712aaedd11743937a63f29e8a630a78e
EunhakLee <admin@return0927.xyz>
2019-10-29 14:13:17 +0900

    Solve BOJ2231
    If N has no creation number, print "0"!!

--- BOJ/2231/2231.py
+++ BOJ/2231/2231.py
@@ -1,8 +1,9 @@
 n = int(input())
 
-creation = lambda n: n + sum([int(x) for x in list(str(n))])
+creation = lambda n: n + sum(map(int, list(str(n))))
 
 for i in range(1, n+1):
     if n == creation(i):
         print(i)
         exit()
+print(0)
