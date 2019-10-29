1cc7e3c5f3ba06262e3281e005a7dbc864479e8c
EunhakLee <admin@return0927.xyz>
2019-10-29 13:21:51 +0900

    Update BOJ2798
    This is brute-force

--- BOJ/2798/2798.py
+++ BOJ/2798/2798.py
@@ -1,26 +1,18 @@
+from itertools import combinations
+
 # N - 카드개수
 # M - 합의 목표
 N, M = [int(x) for x in input().split(' ')]
 cards = [int(x) for x in input().split(' ')]
+cards.sort()
 
 # 파이썬에서는 카드의 수가 필요없는 대신 확신은 하자
 assert len(cards) == N
 
-finding = M / 3
-ind = -1
-for i in range(len(cards) - 1):
-    assert finding >= cards[i]
-    if cards[i] <= finding < cards[i+1]:
-        found = True
-        ind = i
-        break
+last = 0
+for s in combinations(cards, 3):
+    su = sum(s)
+    if M >= su > last:
+        last = su
 
-if ind < 0:  # 만약 기댓값이 카드들에 없다면 맨 끝 3개가 되겠지
-    print(sum(cards[-3:]))
-    exit()
-
-assert ind <= len(cards) - 2  # 끝에 한 개의 원소가 더 있단걸 확인
-for j in range(ind, -1, -1):
-    if sum(cards[j-1:j+2]) <= M:
-        print(sum(cards[j-1:j+2]))
-        break
+print(last)
---
+++ BOJ/2798/README.md
@@ -0,0 +1,9 @@
+# BOJ2798 - None
+
+> [View in BOJ](https://www.acmicpc.net/problem/2798)
+
+## Solutions
+- [Solution on Python](2798.py)
+
+
+_Page built: Oct 29 2019 04:21:12 (W+43, UTC)_
